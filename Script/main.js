d76dc919b5f5b2e569fdae4b1f1699d2241450ac
gitHazik <hazikkhalid700@gmail.com>
2026-06-14 22:57:51 +0530

    init

---
+++ Script/main.js
@@ -0,0 +1,22 @@
+browser.onclick=()=>{
+openWindow(
+"Tor Browser",
+`
+<input placeholder="Search..."
+style="width:95%;padding:12px">
+
+<div style="margin-top:20px">
+<h2>Welcome to Tor Browser</h2>
+<p>Private browsing simulation.</p>
+<p>Try searching:</p>
+
+<ul>
+<li>linux</li>
+<li>wikipedia</li>
+<li>open source</li>
+</ul>
+</div>
+`
+);
+}
+
